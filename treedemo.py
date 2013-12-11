204ea2a90dc62ce818f57a9a1073348f6d550b8b
djf <dragondjf@gmail.com>
2013-12-11 23:21:14 +0800

    chang data

--- treedemo.py
+++ treedemo.py
@@ -4,104 +4,104 @@ from PySide import QtCore
 from PySide import QtGui
 import json
 
-# vehicleData = {
-#     'speed': 80.0,
-#     'rpm': 5000,
-#     'fuelLevel': 0.2,
-#     'fuelLevel_State': "UNKNOWN",
-#     'instantFuelConsumption': 2.2,
-#     'tirePressure': "UNKNOWN",
-#     'beltStatus': {
-#         'driverBeltDeployed': "NOT_SUPPORTED",
-#         'passengerBeltDeployed': "NOT_SUPPORTED",
-#         'passengerBuckleBelted': "NOT_SUPPORTED",
-#         'driverBuckleBelted': "NOT_SUPPORTED",
-#         'leftRow2BuckleBelted': "NOT_SUPPORTED",
-#         'passengerChildDetected': "NOT_SUPPORTED",
-#         'rightRow2BuckleBelted': "NOT_SUPPORTED",
-#         'middleRow2BuckleBelted': "NOT_SUPPORTED",
-#         'middleRow3BuckleBelted': "NOT_SUPPORTED",
-#         'leftRow3BuckleBelted': "NOT_SUPPORTED",
-#         'rightRow3BuckleBelted': "NOT_SUPPORTED",
-#         'leftRearInflatableBelted': "NOT_SUPPORTED",
-#         'rightRearInflatableBelted': "NOT_SUPPORTED",
-#         'middleRow1BeltDeployed': "NOT_SUPPORTED",
-#         'middleRow1BuckleBelted': "NOT_SUPPORTED"
-#     },
-#     'bodyInformation': {
-#         'parkBrakeActive': False,
-#         'ignitionStableStatus': "MISSING_FROM_TRANSMITTER",
-#         'ignitionStatus': "UNKNOWN"
-#     },
-#     'deviceStatus': {
-#         'voiceRecOn': False,
-#         'btIconOn': False,
-#         'callActive': False,
-#         'phoneRoaming': False,
-#         'textMsgAvailable': False,
-#         'battLevelStatus': "NOT_PROVIDED",
-#         'stereoAudioOutputMuted': False,
-#         'monoAudioOutputMuted': False,
-#         'signalLevelStatus': "NOT_PROVIDED",
-#         'primaryAudioSource': "NO_SOURCE_SELECTED",
-#         'eCallEventActive': False
-#     },
-#     'driverBraking': "NOT_SUPPORTED",
-#     'wiperStatus': "NO_DATA_EXISTS",
-#     'headLampStatus': {
-#         "lowBeamsOn": False,
-#         "highBeamsOn": False
-#     },
-#     'engineTorque': 2.5,
-#     'accPedalPosition': 0.5,
-#     'steeringWheelAngle': 1.2,
-#     'myKey': {
-#         "e911Override": "NO_DATA_EXISTS"
-#     },
-#     'avgFuelEconomy': 0.1,
-#     'batteryVoltage': 12.5,
-#     'externalTemperature': 40.0,
-#     'vin': '52-452-52-752',
-#     'prndl': 'PARK',
-#     'batteryPackVoltage': 12.5,
-#     'batteryPackCurrent': 7.0,
-#     'batteryPackTemperature': 30,
-#     'engineTorque': 650,
-#     'tripOdometer': 0,
-#     'genericbinary': '165165650',
-#     'satRadioESN': "165165650",
-#     'rainSensor': 165165650,
-#     'gps': {
-#         'longitudeDegrees': 423293,
-#         'latitudeDegrees': -830464,
-#         'utcYear': 2013,
-#         'utcMonth': 2,
-#         'utcDay': 14,
-#         'utcHours': 13,
-#         'utcMinutes': 16,
-#         'utcSeconds': 54,
-#         'compassDirection': 'SOUTHWEST',
-#         'pdop': 15,
-#         'hdop': 5,
-#         'vdop': 30,
-#         'actual': False,
-#         'satellites': 8,
-#         'dimension': '2D',
-#         'altitude': 7,
-#         'heading': 173,
-#         'speed': 2
-#     },
-#     'a':{
-#         1: 1111,
-#         'b': {
-#             2: 22222,
-#             'c': 3333
-#         }
-#     }
-# }
+vehicleData = {
+    'speed': 80.0,
+    'rpm': 5000,
+    'fuelLevel': 0.2,
+    'fuelLevel_State': "UNKNOWN",
+    'instantFuelConsumption': 2.2,
+    'tirePressure': "UNKNOWN",
+    'beltStatus': {
+        'driverBeltDeployed': "NOT_SUPPORTED",
+        'passengerBeltDeployed': "NOT_SUPPORTED",
+        'passengerBuckleBelted': "NOT_SUPPORTED",
+        'driverBuckleBelted': "NOT_SUPPORTED",
+        'leftRow2BuckleBelted': "NOT_SUPPORTED",
+        'passengerChildDetected': "NOT_SUPPORTED",
+        'rightRow2BuckleBelted': "NOT_SUPPORTED",
+        'middleRow2BuckleBelted': "NOT_SUPPORTED",
+        'middleRow3BuckleBelted': "NOT_SUPPORTED",
+        'leftRow3BuckleBelted': "NOT_SUPPORTED",
+        'rightRow3BuckleBelted': "NOT_SUPPORTED",
+        'leftRearInflatableBelted': "NOT_SUPPORTED",
+        'rightRearInflatableBelted': "NOT_SUPPORTED",
+        'middleRow1BeltDeployed': "NOT_SUPPORTED",
+        'middleRow1BuckleBelted': "NOT_SUPPORTED"
+    },
+    'bodyInformation': {
+        'parkBrakeActive': False,
+        'ignitionStableStatus': "MISSING_FROM_TRANSMITTER",
+        'ignitionStatus': "UNKNOWN"
+    },
+    'deviceStatus': {
+        'voiceRecOn': False,
+        'btIconOn': False,
+        'callActive': False,
+        'phoneRoaming': False,
+        'textMsgAvailable': False,
+        'battLevelStatus': "NOT_PROVIDED",
+        'stereoAudioOutputMuted': False,
+        'monoAudioOutputMuted': False,
+        'signalLevelStatus': "NOT_PROVIDED",
+        'primaryAudioSource': "NO_SOURCE_SELECTED",
+        'eCallEventActive': False
+    },
+    'driverBraking': "NOT_SUPPORTED",
+    'wiperStatus': "NO_DATA_EXISTS",
+    'headLampStatus': {
+        "lowBeamsOn": False,
+        "highBeamsOn": False
+    },
+    'engineTorque': 2.5,
+    'accPedalPosition': 0.5,
+    'steeringWheelAngle': 1.2,
+    'myKey': {
+        "e911Override": "NO_DATA_EXISTS"
+    },
+    'avgFuelEconomy': 0.1,
+    'batteryVoltage': 12.5,
+    'externalTemperature': 40.0,
+    'vin': '52-452-52-752',
+    'prndl': 'PARK',
+    'batteryPackVoltage': 12.5,
+    'batteryPackCurrent': 7.0,
+    'batteryPackTemperature': 30,
+    'engineTorque': 650,
+    'tripOdometer': 0,
+    'genericbinary': '165165650',
+    'satRadioESN': "165165650",
+    'rainSensor': 165165650,
+    'gps': {
+        'longitudeDegrees': 423293,
+        'latitudeDegrees': -830464,
+        'utcYear': 2013,
+        'utcMonth': 2,
+        'utcDay': 14,
+        'utcHours': 13,
+        'utcMinutes': 16,
+        'utcSeconds': 54,
+        'compassDirection': 'SOUTHWEST',
+        'pdop': 15,
+        'hdop': 5,
+        'vdop': 30,
+        'actual': False,
+        'satellites': 8,
+        'dimension': '2D',
+        'altitude': 7,
+        'heading': 173,
+        'speed': 2
+    },
+    'a':{
+        1: 1111,
+        'b': {
+            2: 22222,
+            'c': 3333
+        }
+    }
+}
 
-# with open('data.json', 'w') as f:
-#     json.dump(vehicleData, f, indent=4)
+with open('data.json', 'w') as f:
+    json.dump(vehicleData, f, indent=4)
 
 class TreeItem(object):
     
