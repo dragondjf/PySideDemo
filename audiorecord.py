9a6ee520666765929d0f650afa839f87c4b5b96e
djf <dragondjf@gmail.com>
2013-12-18 00:09:10 +0800

    audio thread

--- audiorecord.py
+++ audiorecord.py
@@ -3,93 +3,124 @@ from PySide import QtCore
 from PySide import QtGui
 
 
-class AudioIODevice(QtCore.QIODevice):
+class ALESingle(QtCore.QObject):
+
+    notification = QtCore.Signal(list)
+
+    def __init__(self):
+        super(ALESingle, self).__init__()
+
+
+alesingle = ALESingle()
+
+
+class AudioRecordThread(QtCore.QThread):
 
     def __init__(self, parent=None):
-        super(AudioIODevice, self).__init__(parent)
-        print self.open(QtCore.QIODevice.ReadWrite)
-        self.readyRead.connect(self.handledata)
-    # def writeData(self, data, s):
-    #     a = self.write(data)
-    #     # print type(data), s
-    #     return a
-
-    def handledata(self):
-        print '------------'
-
-
-class AudioRecord(QtGui.QDialog):
-
-    def __init__(self, parent=None):
-        super(AudioRecord, self).__init__(parent)
+        super(AudioRecordThread, self).__init__(parent)
         self.initAudio()
-        self.initUI()
+        self.databuffer = []
+
+        self.terminated.connect(self.senddata)
 
     def initAudio(self):
-        self.audiofile = QtCore.QFile()
-        self.audiofile.setFileName("testMicIn.wav")
-        self.audiofile.open(QtCore.QIODevice.ReadWrite | QtCore.QIODevice.Truncate)
-        self.audiofile.readyRead.connect(self.handledata)
-
         format = QtMultimedia.QAudioFormat()
         format.setChannelCount(1)
         format.setSampleRate(96000)
         format.setSampleSize(16)
         format.setCodec("audio/pcm")
-        format.setByteOrder(QtMultimedia.QAudioFormat.LittleEndian)
         format.setSampleType(QtMultimedia.QAudioFormat.UnSignedInt)
 
         self.audiodevice = QtMultimedia.QAudioDeviceInfo.defaultInputDevice()
-        # print self.audiodevice
-        # print self.audiodevice.isFormatSupported(format)
         if not self.audiodevice.isFormatSupported(format):
             format = self.audiodevice.nearestFormat(format)
 
-        self.iodevice  = AudioIODevice()
-
         self.audio = QtMultimedia.QAudioInput(self.audiodevice, format, self)
-        self.audio.stateChanged.connect(self.handlestateChange)
+        self.audio.setNotifyInterval(1000)
+        self.audio.notify.connect(self.senddata)
+
+    def run(self):
+        self.iodevice = self.audio.start()
+        self.iodevice.readyRead.connect(self.handleReadyRead)
+        self.exec_()
+
+    def handleReadyRead(self):
+        self.databuffer.append(self.iodevice.readAll().data())
+
+    def senddata(self):
+        # print len(self.databuffer)
+        # data = ''.join(self.databuffer)
+        alesingle.notification.emit(self.databuffer)
+        self.databuffer = []
+
+
+class AudioRecord(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        super(AudioRecord, self).__init__(parent)
+        self.audiorecordthread = AudioRecordThread()
+        self.initUI()
+
+        self.timer = QtCore.QTimer()
+        self.timer.setSingleShot(True)
+        self.timer.setInterval(10000)
+        self.timer.timeout.connect(self.stopRecording)
+        self.timer.start()
+
+    def initUI(self):
+        pass
+
+    def startRecording(self):
+        self.audiorecordthread.start()
+
+    def stopRecording(self):
+        self.close()
+
+    def closeEvent(self, event):
+        self.audiorecordthread.audio.stop()
+        # self.audiorecordthread.exit(0)
+        self.audiorecordthread.terminate()
+        self.timer.stop()
+        print self.audiorecordthread.isFinished()
+
+
+class Mainwindow(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        super(Mainwindow, self).__init__(parent)
+        self.initUI()
 
     def initUI(self):
         self.startButton = QtGui.QPushButton("start")
-        self.pauseButton = QtGui.QPushButton("pause")
         self.stopButton = QtGui.QPushButton("stop")
         mainlayout = QtGui.QHBoxLayout()
         mainlayout.addWidget(self.startButton)
-        mainlayout.addWidget(self.pauseButton)
+        # mainlayout.addWidget(self.pauseButton)
         mainlayout.addWidget(self.stopButton)
         self.setLayout(mainlayout)
 
         self.startButton.clicked.connect(self.startRecording)
-        self.pauseButton.clicked.connect(self.pasueRecording)
         self.stopButton.clicked.connect(self.stopRecording)
 
-    def startRecording(self):
-        self.audio.start(self.audiofile)
-        # self.audio.start(self.iodevice)
+        alesingle.notification.connect(self.handledata)
 
-    def pasueRecording(self):
-        self.audio.suspend()
+    def startRecording(self):
+        self.audiodialog = AudioRecord()
+        self.audiodialog.startRecording()
+        self.audiodialog.show()
 
     def stopRecording(self):
-        self.audio.stop()
-        self.audiofile.close()
-        # del self.audio
+        if hasattr(self, 'audiodialog'):
+            self.audiodialog.close()
+            del self.audiodialog
 
-    def handledata(self):
-        print '..........'
+    @QtCore.Slot(list)
+    def handledata(self, data):
+        print len(data), len(''.join(data))
 
-    def handlestateChange(self, state):
-        print state
-        # print self.iodevice.readAll()
-        # print self.audiofile.flush()
-
-    def closeEvent(self, event):
-        self.stopRecording()
 
 
 def getAvailableDevices():
-    devicenames = {}
     devices = QtMultimedia.QAudioDeviceInfo.availableDevices(QtMultimedia.QAudio.AudioInput)
     for dev in devices:
         print dev.supportedCodecs()
@@ -104,6 +135,6 @@ if __name__ == '__main__':
     import sys
     # print getAvailableDevices()
     app = QtGui.QApplication(sys.argv)
-    main = AudioRecord()
+    main = Mainwindow()
     main.show()
     sys.exit(app.exec_())
