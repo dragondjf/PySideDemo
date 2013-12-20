f170fb8751fe24125bac1265ac22dc72f0651b4d
dingjiangfeng <dingjiangfeng@beyondsoft.com>
2013-12-20 18:09:08 +0800

    add log window

---
+++ DQTextBrowser.py
@@ -0,0 +1,67 @@
+import os
+import Log
+from Log import logSignal 
+import logging
+from PySide import QtGui
+from PySide import QtCore
+
+logger = logging.getLogger(__file__)
+
+class QLogBrowser(QtGui.QTextBrowser):
+
+    style = '''
+    QTextEdit{
+        border-image:url(View/skin/PNG/feedbackinput.png);
+        color:rgb(255,255,255);
+        border:none;
+        background-color:transparent;
+        font-size:14px;
+    }
+    '''
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        super(QLogBrowser, self).__init__(parent)
+        self.resize(500, 500)
+        # print self.style
+        self.setStyleSheet(self.style)
+        logSignal.onelogsin.connect(self.addOneLog)
+        logger.info('1111111111111')
+
+    def addOneLog(self, onemessage):
+        self.append(onemessage)
+
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.resize(800, 600)
+        self.initUI()
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.addlogbytimer)
+        self.timer.start()
+
+    def initUI(self):
+        self.logbrowser = QLogBrowser()
+        self.logbutton = QtGui.QPushButton("Log", self)
+        self.logbutton.move(100, 100)
+        self.logbutton.clicked.connect(self.logshow)
+
+    def logshow(self):
+        self.logbrowser.show()
+
+    def addlogbytimer(self):
+        # logger.debug('*' * 50)
+        logger.info('*' * 50)
+        logger.warning('*' * 50)
+        logger.error('*' * 50)
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
---
+++ Log/__init__.py
@@ -0,0 +1,75 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from PySide import QtCore
+
+
+
+class LogSignal(QtCore.QObject):
+    onelogsin = QtCore.Signal(str)
+    def __init__(self):
+        super(LogSignal, self).__init__()
+        
+
+logSignal = LogSignal()
+
+
+class LoggerHandler(logging.Handler):
+    def __init__(self):
+        super(LoggerHandler, self).__init__()
+
+    def emit(self, record):
+        # onemessage = ' '.join([record.asctime, record.levelname, record.module, record.filename, str(record.lineno), record.msg]) 
+        # c = "<font color='green'>%s</font>" % onemessage
+        onemessage = self.messageFormat(record)
+        logSignal.onelogsin.emit(onemessage)
+
+    def messageFormat(self, record):
+        levelcolors = {
+            'DEBUG': "gray",
+            'INFO': "green",
+            'WARNING': "orange",
+            "ERROR": "red"
+        }
+        colors = {
+        'asctime': 'white',
+        'module': "gray",
+        "filename": 'white',
+        "lineno": "white",
+        "msg": "white",
+        }
+
+        msgitems = ['asctime', 'levelname', 'module', 'filename', 'lineno', 'msg']
+        msglist = []
+        for item in msgitems:
+            # print colors[item]
+            # print getattr(record, msgitems[item])
+            if item is "levelname":
+                msglist.append("<font color=%s>%s</font>" % (levelcolors[str(getattr(record, item))], str(getattr(record, item))))
+            else:
+                msglist.append("<font color=%s>%s</font>" % (colors[item], str(getattr(record, item))))
+        message = '     '.join(msglist)
+        return message
+
+
+logging.root.setLevel(logging.DEBUG)
+logging.root.propagate = 0
+#log write in file
+logpath = os.sep.join([os.getcwd(), 'Log', 'main.log'])
+fh = RotatingFileHandler(logpath, maxBytes=10 * 1024 * 1024, backupCount=100)
+fh.setLevel(logging.INFO)
+#log write in console
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+wh = LoggerHandler()
+wh.setLevel(logging.DEBUG)
+#log formatter
+formatter = logging.Formatter('%(asctime)s %(levelname)8s [%(filename)16s:%(lineno)04s] %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logging.root.addHandler(fh)
+logging.root.addHandler(ch)
+logging.root.addHandler(wh)
+logger = logging.root
+logger.propagate = 0
