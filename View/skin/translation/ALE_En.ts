e25402a8fbb1426f8d8514d2df6512b8a8767164
dingjiangfeng <dingjiangfeng@beyondsoft.com>
2014-03-20 14:41:44 +0800

    update skin pngs111

---
+++ View/skin/translation/ALE_En.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>ALEMenuBar</name>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="262"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="266"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="293"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="301"/>
+        <source>Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="305"/>
+        <source>MFD3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="313"/>
+        <source>MFD4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="322"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="332"/>
+        <source>Search Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="341"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="351"/>
+        <source>Vehicle Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="357"/>
+        <source>DID Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="363"/>
+        <source>DTC Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="393"/>
+        <source>About ALE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="399"/>
+        <source>Feedback to us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="272"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="389"/>
+        <source> Help </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="410"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="375"/>
+        <source>Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="412"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="View/Dialog/AboutDialog.py" line="74"/>
+        <source>About Us</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AppScreen</name>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="205"/>
+        <source>Find new apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="206"/>
+        <source>App settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="209"/>
+        <source>Mobile Apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="143"/>
+        <source>Searching...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="154"/>
+        <source>additional
+App(s) found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="195"/>
+        <source>Connecting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="152"/>
+        <source>No additional
+Apps found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="49"/>
+        <source>Find new Apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AudioRecordDialog</name>
+    <message>
+        <location filename="View/Dialog/AudioRecordDialog.py" line="110"/>
+        <source>Voice Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CommandScreen</name>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="52"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="58"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="61"/>
+        <source>Exit </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DIDDialog</name>
+    <message>
+        <location filename="View/Dialog/DidDialog.py" line="63"/>
+        <source>DID Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DTCDialog</name>
+    <message>
+        <location filename="View/Dialog/DtcDialog.py" line="63"/>
+        <source>DTC Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeedbackusDidalog</name>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="133"/>
+        <source>Feedback to us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="139"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="143"/>
+        <source>Email:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="148"/>
+        <source>Telephone:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="152"/>
+        <source>IM:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="158"/>
+        <source>Feedback:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="162"/>
+        <source>Feedback is required.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstSettingDialog</name>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="97"/>
+        <source>Server IP: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="100"/>
+        <source>Apply Now: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="107"/>
+        <source>Server Port: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="93"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JsonTreeDialog</name>
+    <message>
+        <location filename="View/Dialog/JsonTreeDialog.py" line="417"/>
+        <source>Vehicle Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MFDScreen</name>
+    <message>
+        <location filename="View/Screen/MFDScreen.py" line="538"/>
+        <source>More</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="View/__init__.py" line="466"/>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="473"/>
+        <source>Choice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="480"/>
+        <source>Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="453"/>
+        <source>Searching...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PerformInteractionDialog</name>
+    <message>
+        <location filename="View/Dialog/PerformInteractionDialog.py" line="40"/>
+        <source>Choose</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Scrollable</name>
+    <message>
+        <location filename="View/Screen/Scrollable.py" line="130"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ View/skin/translation/ALE_Zh.ts
@@ -0,0 +1,337 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
+<context>
+    <name>ALEMenuBar</name>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="262"/>
+        <source>File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="266"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="293"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="301"/>
+        <source>Screen</source>
+        <translation type="unfinished">显示屏</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="305"/>
+        <source>MFD3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="313"/>
+        <source>MFD4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="322"/>
+        <source>Device</source>
+        <translation type="unfinished">设备</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="304"/>
+        <source>Enable Bluetooth</source>
+        <translation type="obsolete">启用蓝牙</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="332"/>
+        <source>Search Devices</source>
+        <translation type="unfinished">查找设备</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="341"/>
+        <source>View</source>
+        <translation type="unfinished">视图</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="351"/>
+        <source>Vehicle Data</source>
+        <translation type="unfinished">汽车数据</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="357"/>
+        <source>DID Data</source>
+        <translation type="unfinished">DID数据</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="363"/>
+        <source>DTC Data</source>
+        <translation type="unfinished">DTC数据</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="393"/>
+        <source>About ALE</source>
+        <translation type="unfinished">关于ALE</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="399"/>
+        <source>Feedback to us</source>
+        <translation type="unfinished">反馈我们</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="272"/>
+        <source>Language</source>
+        <translation type="unfinished">语言</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="389"/>
+        <source> Help </source>
+        <translation type="unfinished">帮助</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="410"/>
+        <source>Start</source>
+        <translation type="unfinished">开始</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="375"/>
+        <source>Report</source>
+        <translation type="unfinished">报告</translation>
+    </message>
+    <message>
+        <location filename="View/MenuBar/__init__.py" line="412"/>
+        <source>Stop</source>
+        <translation type="unfinished">结束</translation>
+    </message>
+</context>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="View/Dialog/AboutDialog.py" line="74"/>
+        <source>About Us</source>
+        <translation type="unfinished">关于我们</translation>
+    </message>
+</context>
+<context>
+    <name>AppScreen</name>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="205"/>
+        <source>Find new apps</source>
+        <translation type="unfinished">查找应用程序</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="206"/>
+        <source>App settings</source>
+        <translation type="unfinished">应用程序设置</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="209"/>
+        <source>Mobile Apps</source>
+        <translation type="unfinished">移动应用</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="143"/>
+        <source>Searching...</source>
+        <translation type="unfinished">正在搜索......</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="152"/>
+        <source>No additional
+Apps found</source>
+        <translation type="unfinished">未找到其他
+移动应用程序</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="154"/>
+        <source>additional
+App(s) found</source>
+        <translation type="unfinished"> 个其他应用程序 
+找到</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="195"/>
+        <source>Connecting</source>
+        <translation type="unfinished">正在连接</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/AppScreen.py" line="49"/>
+        <source>Find new Apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AudioRecordDialog</name>
+    <message>
+        <location filename="View/Dialog/AudioRecordDialog.py" line="110"/>
+        <source>Voice Record</source>
+        <translation type="unfinished">录音</translation>
+    </message>
+</context>
+<context>
+    <name>CommandScreen</name>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="52"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="58"/>
+        <source>Menu</source>
+        <translation type="unfinished">菜单</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/CommandScreen.py" line="61"/>
+        <source>Exit </source>
+        <translation type="unfinished">退出 </translation>
+    </message>
+</context>
+<context>
+    <name>DIDDialog</name>
+    <message>
+        <location filename="View/Dialog/DidDialog.py" line="63"/>
+        <source>DID Data</source>
+        <translation type="unfinished">DID数据</translation>
+    </message>
+</context>
+<context>
+    <name>DTCDialog</name>
+    <message>
+        <location filename="View/Dialog/DtcDialog.py" line="63"/>
+        <source>DTC Data</source>
+        <translation type="unfinished">DTC数据</translation>
+    </message>
+</context>
+<context>
+    <name>FeedbackusDidalog</name>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="133"/>
+        <source>Feedback to us</source>
+        <translation type="unfinished">反馈我们</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="139"/>
+        <source>Name:</source>
+        <translation type="unfinished">姓名：</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="143"/>
+        <source>Email:</source>
+        <translation type="unfinished">邮箱：</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="148"/>
+        <source>Telephone:</source>
+        <translation type="unfinished">电话：</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="152"/>
+        <source>IM:</source>
+        <translation type="unfinished">即时通信：</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="158"/>
+        <source>Feedback:</source>
+        <translation type="unfinished">反馈：</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FeedbackusDialog.py" line="162"/>
+        <source>Feedback is required.</source>
+        <translation type="unfinished">反馈不能为空</translation>
+    </message>
+</context>
+<context>
+    <name>FirstSettingDialog</name>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="97"/>
+        <source>Server IP: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="100"/>
+        <source>Apply Now: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="107"/>
+        <source>Server Port: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/FirstSettingDialog.py" line="93"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+</context>
+<context>
+    <name>FloatWindow</name>
+    <message>
+        <location filename="View/FloatWindows/__init__.py" line="45"/>
+        <source>View</source>
+        <translation type="obsolete">视图</translation>
+    </message>
+</context>
+<context>
+    <name>JsonTreeDialog</name>
+    <message>
+        <location filename="View/Dialog/JsonTreeDialog.py" line="409"/>
+        <source>Custom Data</source>
+        <translation type="obsolete">自定义数据</translation>
+    </message>
+    <message>
+        <location filename="View/Dialog/JsonTreeDialog.py" line="417"/>
+        <source>Vehicle Data</source>
+        <translation type="unfinished">汽车数据</translation>
+    </message>
+</context>
+<context>
+    <name>MFDScreen</name>
+    <message>
+        <location filename="View/Screen/MFDScreen.py" line="473"/>
+        <source>More...</source>
+        <translation type="obsolete">更多...</translation>
+    </message>
+    <message>
+        <location filename="View/Screen/MFDScreen.py" line="538"/>
+        <source>More</source>
+        <translation type="unfinished">更多</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="View/__init__.py" line="466"/>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="473"/>
+        <source>Choice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="480"/>
+        <source>Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View/__init__.py" line="453"/>
+        <source>Searching...</source>
+        <translation type="unfinished">正在搜索......</translation>
+    </message>
+</context>
+<context>
+    <name>PerformInteractionDialog</name>
+    <message>
+        <location filename="View/Dialog/PerformInteractionDialog.py" line="40"/>
+        <source>Choose</source>
+        <translation type="unfinished">选择</translation>
+    </message>
+</context>
+<context>
+    <name>Scrollable</name>
+    <message>
+        <location filename="View/Screen/Scrollable.py" line="130"/>
+        <source>Close</source>
+        <translation type="unfinished">关闭</translation>
+    </message>
+</context>
+</TS>
